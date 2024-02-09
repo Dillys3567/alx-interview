62faad7ac55df6b961291ad1aa4f1d5471fc7f6c
Dillys Annan <80262558+Dillys3567@users.noreply.github.com>
2024-02-09 15:43:01 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-from math import factorial
-
 """
 Returns a list of integers of Pascal's triangle
 """
 
+def factorial(n):
+	"""
+ 	Find the factorial of a number
+  	"""
+	if (n == 0):
+		return 1
+	fact = 1
+	for i in range(1, n+1):
+		fact*=i
+	return fact
+		       
 def pascal_triangle(n):
 	"""
 	Return list of integers
