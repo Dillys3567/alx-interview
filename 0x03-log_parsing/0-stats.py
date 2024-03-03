e8f0455f8787a0b0c7cc5df9ad52df3d02425950
Dillys Annan <dillysannan@gmail.com>
2024-03-03 21:11:03 +0000

    log parse

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys
+"""Script to read from stdin line by line and computer metrics"""
+
+
+def printMetrics(dic, size):
+    """Print information"""
+    print("File size: {:d}".format(size))
+    for code in sorted(dic.keys()):
+        if dic[code] != 0:
+            print("{}: {:d}".format(code, dic[code]))
+
+
+codes = {"200":0, "301":0,"400" :0, "401":0, "403":0, "404":0, "405":0, "500":0}
+
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+           printMetrics(codes,size)
+
+        input = line.split()
+        count += 1
+
+        try:
+            size += int(input[-1])
+        except:
+            pass
+
+        try:
+            if input[-2] in codes:
+                codes[input[-2]] += 1
+        except:
+            pass
+    printMetrics(codes,size)
+
+
+except KeyboardInterrupt:
+    printMetrics(codes,size)
+    raise
+
+
