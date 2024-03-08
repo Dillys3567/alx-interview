cc0bba0a8f07dbe7ca6c7cb8e04213eecf782a54
Dillys Annan <dillysannan@gmail.com>
2024-03-08 01:33:42 +0000

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,15 +4,31 @@ Script to check for valid UTF-8 encodeing
 """
 
 def validUTF8(data):
-    isUTF8 = False
     """Return true if data is valid UTF-8 encoding, else return False"""
     new_data = [convertToBinary(x) for x in data]
-    n = len(new_data)
+    #n = len(new_data)
     if new_data[0][0] == '0':
-        isUTF8 = True
-    return isUTF8
+        return True
+    else:
+        n = check(new_data[0])
+        length = len(new_data)
+        if n != (length - 1):
+            return False
+        for i in range(1,length -1):
+            x = check(new_data[i])
+            if x != 1:
+                return False
+    return True
     
-    
+def check(num):
+    i = 0
+    mask = 10000000
+    num = int(num)
+    while mask & num:
+        mask >>=1
+        i+=1
+    return i
+
     
 
 
